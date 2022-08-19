4718dd94921d5e232b09717c606aa44df8509d65
Ricardo Miguel Pereyra <91433040+RMPAR@users.noreply.github.com>
2022-08-19 04:04:29 -0300

    varias modificaciones a todos los archivos del proyecto

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const inputText = document.querySelector('.input-text');
 const message = document.querySelector('.message');
 
-function encript(stringEncrypted) {
+function buttonEncrypt() {
+	const textDecrypted = encrypt(inputText.value);
+	message.value = textDecrypted;
+	message.style.backgroundImage = 'none';
+	inputText.value = '';
+}
+
+function buttonDecrypt() {
+	const textEncrypted = decrypt(inputText.value);
+	message.value = textEncrypted;
+	//message.style.backgroundImage = 'none';
+	inputText.value = '';
+}
+
+function encrypt(stringEncrypted) {
 	let matrixCode = [
+		['a', 'ai'],
 		['e', 'enter'],
 		['i', 'imes'],
-		['a', 'ai'],
 		['o', 'ober'],
 		['u', 'ufat'],
 	];
@@ -14,7 +28,7 @@ function encript(stringEncrypted) {
 
 	for (let i = 0; i < matrixCode.length; i++) {
 		if (stringEncrypted.includes(matrixCode[i][0])) {
-			stringDecrypted = stringDecrypted.replaceAll(
+			stringEnrypted = stringEncrypted.replaceAll(
 				matrixCode[i][0],
 				matrixCode[i][1]
 			);
@@ -22,4 +36,27 @@ function encript(stringEncrypted) {
 	}
 
 	return stringEncrypted;
+}
+
+function decrypt(stringDecrypted) {
+	let matrixCode = [
+		['a', 'ai'],
+		['e', 'enter'],
+		['i', 'imes'],
+		['o', 'ober'],
+		['u', 'ufat'],
+	];
+
+	stringDecrypted = stringDecrypted.toLowerCase();
+
+	for (let i = 0; i < matrixCode.length; i++) {
+		if (stringDecrypted.includes(matrixCode[i][1])) {
+			stringDecrypted = stringDecrypted.replaceAll(
+				matrixCode[i][1],
+				matrixCode[i][0]
+			);
+		}
+	}
+
+	return stringDecrypted;
 }
